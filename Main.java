4ea0b4c1a07a96a8808407704edf232c6a86833f
fiscn <fiscn2709@wrdsb.ca>
2020-10-08 15:31:06 +0000

    Finished A6 Q2 program

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner; 
+
 /**
  *
- * @author 
+ * @author Neil Fischer
  */
 public class Main {
 
@@ -9,7 +11,32 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //Create scanner for user input 
+    Scanner input = new Scanner(System.in); 
+
+    //ask the user to enter 10 integers 
+    System.out.println("Please enter in 10 integers to put into the array"); 
+
+    //create array for integers 
+    int[] numB = new int[10]; 
+
+    //use for loop to get all the values for arrays 
+    for(int i = 0; i < numB.length; i++){
+      numB[i] = input.nextInt(); 
+    }  
+
+    //prompt the user to enter a number to find 
+    System.out.println("Please enter a number to find"); 
+
+    //create variable for number the user wants to find 
+    int searchNum = input.nextInt();
+
+    //create for statement to find and print the number the user is looking for 
+    for(int i = 0; i < numB.length; i++){
+      if(numB[i] == searchNum){
+        System.out.println(searchNum + " is located at index " + i);
+      }
+    }
     
   }
 }
